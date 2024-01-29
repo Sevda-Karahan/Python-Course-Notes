8917470b65c24bfe00e95308788063c80a6eadb3
Sevda KARAHAN <116480291+Sevda-Karahan@users.noreply.github.com>
2024-01-29 15:27:56 +0300

    Update 03_Files.py

--- Python-DataStructures/03_Files.py
+++ Python-DataStructures/03_Files.py
@@ -96,3 +96,16 @@ for line in fhand:
     if line.startswith('Subject:'):
         count += 1
 print('There were', count, 'subject lines in', fname)
+
+############################################################
+
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File', fname, 'cannot be opened')
+    quit()
+for line in fhand:
+    line = line.rstrip()
+    line = line.upper()
+    print(line)
